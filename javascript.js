780604317f214ab5fa131929cdf3333c3d2ab9c6
kavin2001 <86868712+kavin2001@users.noreply.github.com>
2022-04-20 19:11:18 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+//number
+//strings
+//arrays
+//object
+//variable "var"
+    var no1 = 10 ;
+    var name1 = "cyber bug" ;
+    //var name = "hi" ;
+    alert(name1)
+//alert("hi, this is me wedsite");
